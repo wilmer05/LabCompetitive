21106d7d358d655fa90a745516b909a6438f355d
Wilmer Rafael Bandres Hernandez <wbandres@mahisoft.com>
2018-04-28 13:07:01 +0200

    finishing

--- Lab1/ex2.cpp
+++ Lab1/ex2.cpp
@@ -31,6 +31,24 @@ int main(){
     cin >> t;
     FOR(i,t) {
         cin >> type >> m >> n;
+        if( type == 'Q' ) {
+            if( m > n ) swap(m, n); 
+            if( n <= 2 ) cout << "1" << endl;
+            else if( m == 3 && n == 3) cout << "2" << endl;
+            else cout << min(n,m) << endl;
+        } else if (type == 'K') {
+            cout << ((n + 1) / 2) * ((m + 1) / 2) << endl;
+        } else if (type == 'r') {
+            cout << min(m, n) << endl; 
+        } else {
+            int white, blacks;
+            white = blacks = 0;
+            white = ((n + 1) / 2 ) * ((m + 1) / 2);
+            white += (n / 2) * (m / 2);
+            blacks = white + n / 2 - ( m & 1 ? (n + 1 ) / 2 : (n ) / 2 );
+            cout << max(white, blacks) << endl;
+        
+        }
 
     }
 
