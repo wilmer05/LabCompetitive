d875e03c9f57c041729b4a30b8673ba9f33647e3
Wilmer Rafael Bandres Hernandez <wbandres@mahisoft.com>
2018-05-10 14:50:15 +0200

    commiting dp

--- Lab2/ex7.cpp
+++ Lab2/ex7.cpp
@@ -23,8 +23,35 @@
 
 using namespace std;
 
+int es[40], bs[40], V, T, k;
+const int oo = 1e9 + 10;
+const int mn = 1e5 + 10;
+int memo[mn];
 
 int main(){
+
+    cin >> T;
+    FOR(t,T) {
+        cin >> k >> V;
+        V*=V;
+        memo[V] = oo;
+        FOR(i,k) cin >> bs[i]  >> es[i];
+
+        memo[0] = 0;
+        for (int i = 1;  i < V + 1; i++) {
+            memo[i] = oo;   
+            FOR(j,k) {
+                 int tmp = i - bs[j] * bs[j] - es[j] * es[j];
+                 if(tmp >=0)
+                     memo[i] = min(memo[i], 1 + memo[tmp]);
+            }
+        }
+
+        if(memo[V] != oo) cout << memo[V] << endl;
+        else cout << "impossible\n";
+        
+    }
+    
 
 	return 0;
 }
